75751f119525a37698cf620e44cb36c70706e365
Yash Joshi <yash.joshi76@gmail.com>
2022-05-22 23:06:57 +0100

    key fix

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import React from "react";
 import "./styles.css";
 
 const lads = [
-  { name: "Benny Wood", age: 24 },
-  { name: "Nial Carr", age: 24 },
-  { name: "Aitana Hermana", age: 22 },
-  { name: "Lisa M Gasper", age: 22 }
+  { id: "1", name: "Benny Wood", age: 24 },
+  { id: "2", name: "Nial Carr", age: 24 },
+  { id: "3", name: "Aitana Hermana", age: 22 },
+  { id: "4", name: "Lisa M Gasper", age: 22 }
 ];
 
 const Lads = (props) => {
-  const { name, age } = props;
+  const { name, age, id } = props;
   return (
-    <div>
+    <div key={id}>
       Name : {name} and Age: {age}
     </div>
   );
@@ -21,7 +21,7 @@ export default function App() {
     <div className="App">
       <h1>Hola Amigos</h1>
       {lads.map((ls) => {
-        return <Lads {...ls} />;
+        return <Lads {...ls} key={ls.id} />;
       })}
     </div>
   );
